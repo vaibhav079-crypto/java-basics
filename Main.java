52200d21cc29351e52238eb66f95fa0ad7f6265f
vaibhav079_crypto <vaibhavsinghal079@gmail.com>
2026-05-09 22:18:50 +0530

    switch day

--- Main.java
+++ Main.java
@@ -1,21 +1,45 @@
 import java.util.Scanner;
-//area of rectangle
-class Main {
-
-    public static void main(String[] args) {
-
+class Main{
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter day number");
+        int day = sc.nextInt();
 
-        System.out.println("enter first number:");
-        int l =sc.nextInt();
-        System.out.println("enter second number:");
-        int b = sc.nextInt();
+        switch(day){
+            case 1:
+                System.out.println("monday");
+                break;
 
-        int area = l * b;
+             case 2:
+                System.out.println("tuesday");
+                break;   
+             
+             case 3:
+                System.out.println("wednesday");
+                break;
 
-        
+             case 4:
+                System.out.println("thrusday");
+                break;
+                
+             case 5:
+                System.out.println("friday");
+                break;
+                
+            case 6:
+                System.out.println("saturday");
+                break;
+                
+             case 7:
+                System.out.println("sunday");
+                break;
+                
+            default:
+                System.out.println("invalid day");
+                break;    
+                
 
-        System.out.println("area = " + area);
+        }
 
 
     }
