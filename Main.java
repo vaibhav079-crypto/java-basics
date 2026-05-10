22f0cf4a0d4fe21d3e27022597906451db499f5a
vaibhav079_crypto <vaibhavsinghal079@gmail.com>
2026-05-10 08:58:54 +0530

    gratest of 3 numbers

--- Main.java
+++ Main.java
@@ -1,46 +1,34 @@
 import java.util.Scanner;
-class Main{
-    public static void main(String[]args){
+
+class Main {
+
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter day number");
-        int day = sc.nextInt();
 
-        switch(day){
-            case 1:
-                System.out.println("monday");
-                break;
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
 
-             case 2:
-                System.out.println("tuesday");
-                break;   
-             
-             case 3:
-                System.out.println("wednesday");
-                break;
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
 
-             case 4:
-                System.out.println("thrusday");
-                break;
-                
-             case 5:
-                System.out.println("friday");
-                break;
-                
-            case 6:
-                System.out.println("saturday");
-                break;
-                
-             case 7:
-                System.out.println("sunday");
-                break;
-                
-            default:
-                System.out.println("invalid day");
-                break;    
-                
+        System.out.println("Enter third number:");
+        int c = sc.nextInt();
+
+        if(a >= b && a >= c) {
+
+            System.out.println(a + " is greatest");
 
         }
+        else if(b >= a && b >= c) {
 
+            System.out.println(b + " is greatest");
 
+        }
+        else {
+
+            System.out.println(c + " is greatest");
+
+        }
     }
 }
